264c6defb6e836a51227b64f009d1cb877a1ecb6
lar4 <lar4@lar4-pc.(none)>
2014-08-07 15:22:31 +0300

    Trying to fix GUI crash bug

--- lar_car/src/master.py
+++ lar_car/src/master.py
@@ -1,26 +1,6 @@
 #!/usr/bin/env python
 
 from GUI import car_gui
-import thread,time
-from PIL import ImageTk
-import pygtk
-import gtk,webkit
 
-def keydown(widget, event):
-  #global ros_handler
-  key = gtk.gdk.keyval_name(event.keyval)
-  print "Key %s (%d) was pressed" % (key, event.keyval)
-  #if key == 'KP_0':
-    #ros_handler.speed.set(0)
-    #ros_handler.steer.set(0)
-  #if key == 'Up':
-    #ros_handler.speed.add(0.1)
-  #if key == 'Down':
-    #ros_handler.speed.add(-0.1)
-  #if key == 'Left':
-    #ros_handler.steer.add(0.1)
-  #if key == 'Right':
-    #ros_handler.steer.add(-0.1)
-  #ros_handler.publisher()
 gui_handler = car_gui((800,600),keydown)
 gui_handler.run_main()
