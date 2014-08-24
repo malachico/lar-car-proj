f769c0b33e7b843605537ad24a4c414241c9d1c1
lar5 <lar5@lar5-PC.(none)>
2014-08-24 16:31:01 +0300

    Added the map publisher

--- lar_car/src/master.py
+++ lar_car/src/master.py
@@ -3,4 +3,5 @@
 from GUI import car_gui
 
 gui_handler = car_gui((800,600))
+
 gui_handler.run_main()
--- lowlevelcontrol/src/temp/joystick_controll.py
+++ lowlevelcontrol/src/temp/joystick_controll.py
@@ -1,55 +1,56 @@
-'''
-import pygame
-#import simpleguitk as simplegui
-import time
+#!/usr/bin/env python 
+##import pygame
+##import time
 
-class joystick_control():
-    def __init__(self):
-        pygame.init()
-        pygame.joystick.init()
-        joystick_count = pygame.joystick.get_count()
-        if joystick_count<1:
-            print "Joystick is not connected"
-            return
-        self.joystick = pygame.joystick.Joystick(0)
-        self.joystick.init()
-        self._numofaxes = self.joystick.get_numaxes()
-        self._numofbuttons = self.joystick.get_numbuttons()
-        self._hats = self.joystick.get_numhats()
+##class joystick_control():
+    ##def __init__(self):
+        ##pygame.init()
+        ##pygame.joystick.init()
+        ##joystick_count = pygame.joystick.get_count()
+        ##if joystick_count<1:
+            ##print "Joystick is not connected"
+            ##return
+        ##self.joystick = pygame.joystick.Joystick(0)
+        ##self.joystick.init()
+        ##self._numofaxes = self.joystick.get_numaxes()
+        ##self._numofbuttons = self.joystick.get_numbuttons()
+        ##self._hats = self.joystick.get_numhats()
         
-        self.axes = [0]*self._numofaxes
-        self.buttons = [0]*self._numofbuttons
-        self.hats = [0]*self._hats
+        ##self.axes = [0]*self._numofaxes
+        ##self.buttons = [0]*self._numofbuttons
+        ##self.hats = [0]*self._hats
         
 
-    def get_axes(self):
-        for i in range(self._numofaxes):
-            self.axes[i] = self.joystick.get_axis( i )
-        return self.axes
-    def get_buttons(self):
-        for i in range(self._numofbuttons):
-            self.buttons[i] = self.joystick.get_button( i )
-        return self.buttons    
-    def update_handler(self):
-        pygame.event.get()
-        self.get_buttons() 
-        self.get_axes()
+    ##def get_axes(self):
+        ##for i in range(self._numofaxes):
+            ##self.axes[i] = self.joystick.get_axis( i )
+        ##return self.axes
+    ##def get_buttons(self):
+        ##for i in range(self._numofbuttons):
+            ##self.buttons[i] = self.joystick.get_button( i )
+        ##return self.buttons    
+    ##def update_handler(self):
+        ##pygame.event.get()
+        ##self.get_buttons() 
+        ##self.get_axes()
         
-    def get_all(self):
-        return [self.buttons, self.axes]
-    def test_joystick(self):
-        print "number of joysticks: ", pygame.joystick.get_count()
-        print "number of axes: ", self._numofaxes
-        print "number of buttons: ", self._numofbuttons
-        for i in range(3):
-            but, ax = self.get_all()
-            print "buttons: ", self.get_buttons()
-            print "axes: ", self.get_axes()
-            time.sleep(1)
+    ##def get_all(self):
+        ##return [self.buttons, self.axes]
+    ##def test_joystick(self):
+        ##print "number of joysticks: ", pygame.joystick.get_count()
+        ##print "number of axes: ", self._numofaxes
+        ##print "number of buttons: ", self._numofbuttons
+        ##for i in range(3):
+            ##but, ax = self.get_all()
+            ##print "buttons: ", but
+            ##print "axes: ", ax
+            ##time.sleep(1)
 
-joy = joystick_control()
-joy.test_joystick()
-'''
+##if __name__ == "__main__":
+  ##print 'searching for joysticks'
+  ##joy = joystick_control()
+  ##joy.test_joystick()
+
 import pygame
 
 # Define some colors
